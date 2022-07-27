9d9b1e0365797a6238389e03b5548f00a0301c93
Blake Lein <blake.lein@gmail.com>
2022-07-27 15:32:51 -0500

    Initialized File Structure Day0

---
+++ controller/index.js
@@ -0,0 +1,9 @@
+// Server Imports
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const es6Renderer = require("epress-es6-template-engine");
+
+// // Middle Ware
+// app.use(express.json());
+// app.use(cors());
