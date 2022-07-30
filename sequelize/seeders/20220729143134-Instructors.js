de5a66e444144e1306c5e5cb608a47387e00e9fe
Blake Lein <blake.lein@gmail.com>
2022-07-30 08:14:46 -0500

    commit

--- sequelize/seeders/20220729143134-Instructors.js
+++ sequelize/seeders/20220729143134-Instructors.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,42 +10,43 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
-    await queryInterface.bulkInsert('Instructors',
-    [
-      {
-        firstName: "Blake",
-        lastName: "Lein",
-        email: "blk&cookies@hotmail.com",
-        password: "123123",
-        instrument: "MegaTuba",
-        students: null,
-        lesson: "5:15",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        firstName: "Ethan",
-        lastName: "Gula",
-        email: "ethang@hotmail.com",
-        password: "123123",
-        instrument: "Clarihorn",
-        students: null,
-        lesson: "5:00",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-    ],
-    {}
-  );
-},
+     */
+    await queryInterface.bulkInsert(
+      "Instructors",
+      [
+        {
+          firstName: "Blake",
+          lastName: "Lein",
+          email: "blk&cookies@hotmail.com",
+          password: "123123",
+          instrument: "MegaTuba",
+          students: null,
+          lesson: "5:15",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          firstName: "Ethan",
+          lastName: "Gula",
+          email: "ethang@hotmail.com",
+          password: "123123",
+          instrument: "Clarihorn",
+          students: null,
+          lesson: "5:00",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
