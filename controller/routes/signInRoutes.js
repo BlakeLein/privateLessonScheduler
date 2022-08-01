98b21ed6939b430c3eee1ed6e197e11d8fe0c1ae
Emgula96 <ethan_gula@yahoo.com>
2022-07-31 21:59:46 -0400

    initial sesssion creation
    very sloppy, needs fine tuning

--- controller/routes/signInRoutes.js
+++ controller/routes/signInRoutes.js
@@ -14,38 +14,59 @@ const { Lessons } = require("../../sequelize/models");
 // Middle Ware
 app.use(express.json());
 
+const checkLogin = (req, res, next) => {
+    if (req.session.user){
+      next()
+    } else 
+    {
+      res.json({
+        message:"Login Required"
+      })
+    }
+    }
+
 // Sign In Routes
 router.get("/", (req, res) => {
   res.render("sign-in");
 });
 
-router.post("/signin/student-sign-in", async (req, res) => {
+router.post("/student-sign-in", async (req, res) => {
+  console.log(req.session)
   const { user, pass } = req.body;
   try {
-    const findUser = await Student.findOne({
+    const findUser = await Students.findOne({
       where: {
         email: user,
       },
     });
-    const userWeFound = findUser.dataValues;
-    const validated = await bcrypt.compare(pass, userWeFound.password);
+    // const userWeFound = findUser.dataValues;
+    // const validated = await bcrypt.compare(pass, userWeFound.password);
+    console.log("found user?")
     if (!findUser) {
-      res
-        // If they fail the login do this.
-        .status(400)
-        .send(
+      console.log("helllllo")
+      // If they fail the login do this.
+      // .status(400)
+      res.send(
           "That user does not exist in our database. Did you get the username correct?"
         );
     } else {
+      req.session.findUser = findUser
+      res.json({message:
+        "login sucess",
+      findUser: findUser})
       // If they log in successfully do this.
-      res.status(200).send(findUser.dataValues).redirect("/homepage");
+      // res.status(200).send(findUser.dataValues)
+      // console.log("i work, i think")
+      console.log(req.session.findUser)
     }
   } catch (error) {
+    console.log(error)
+    console.log("oops an erro")
     res.send("That user is not in our system. Please create an account first.");
   }
 });
 
-router.post("/signin/instructor-sign-in", async (req, res) => {
+router.post("/instructor-sign-in", async (req, res) => {
   console.log(req.body);
   const { user, pass } = req.body;
   try {
@@ -71,5 +92,11 @@ router.post("/signin/instructor-sign-in", async (req, res) => {
     res.send("That user is not in our system. Please create an account first.");
   }
 });
+
+
+
+router.post("/instructor-test", checkLogin, async (req,res)=>{
+  res.send("okay homie you work")
+})
 
 module.exports = router;
