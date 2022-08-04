2c26d543b9e1012506d0406afcec4c1de6815a77
Blake Lein <blake.lein@gmail.com>
2022-08-04 10:13:36 -0500

    Updates

--- test.js
+++
@@ -1,8 +0,0 @@
-console.log("Hi");
-const instructor = "Mark Barton";
-console.log(instructor);
-
-instructorSplit = instructor.split(" ");
-for (let i = 0; i < instructorSplit.length; i++) {
-  console.log(instructorSplit[i]);
-}
