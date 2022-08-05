0892618d427750e8c119db40b4af48acb69d2781
Emgula96 <ethan_gula@yahoo.com>
2022-08-04 23:51:42 -0400

    mobile response

--- controller/public/js/home.js
+++ controller/public/js/home.js
@@ -3,3 +3,12 @@ const signInButton = document.getElementById("signInButton");
 signInButton.addEventListener("click", () => {
   window.location.href = "/signin";
 });
+
+function myFunction() {
+  const x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
